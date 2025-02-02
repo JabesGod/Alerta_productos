efd91b4b74d0edda60c1c3cfe84022393746bb1b
JabesGod <99416529+JabesGod@users.noreply.github.com>
2025-02-02 10:18:52 -0500

    Actualizacion Icono de pestaña
    xd

--- poyecto_tecnonacho/urls.py
+++ poyecto_tecnonacho/urls.py
@@ -12,6 +12,4 @@ urlpatterns = [
     path('producto/eliminar/<int:producto_id>/', views.eliminar_producto, name='eliminar_producto'),
     path('producto/toggle_listo/<int:pk>/', views.toggle_listo, name='toggle_listo'),
     path('obtener_descripcion_sku/', views.obtener_descripcion_sku, name='obtener_descripcion_sku'),
-    
-
 ]
