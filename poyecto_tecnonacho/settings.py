7d6887e4f9e24102ef31eb3b72b61bf8fafce61a
Jabes Acuña <99416529+JabesGod@users.noreply.github.com>
2025-02-24 15:38:57 -0500

    Update settings.py

--- poyecto_tecnonacho/settings.py
+++ poyecto_tecnonacho/settings.py
@@ -100,8 +100,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tecnonacho',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'django_alert',
+        'PASSWORD': 'F44FF;]L$!G9Zod',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
